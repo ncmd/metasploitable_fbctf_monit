a3c316f9d6d555a47361fe501572e1f72f64a48e
Charles M. Chong <cchong.vise@gmail.com>
2016-06-09 14:14:55 -0700

    Generate_random_hostname bug fix
    Properly parsed hostname for golden image
    Added Monit ID reset
    Added video temporary demo

--- metasploitable/root/generate_random_hostname.sh
+++ metasploitable/root/generate_random_hostname.sh
@@ -13,21 +13,39 @@ sort -R /root/hostnamelist.txt | head -n 1 > /root/newname.txt
 
 # New hostname is declared by concatenating the contents of /root/newname.txt file
 newhost=$(cat /root/newname.txt)
+newhost=$newhost$RANDOM
 
 # Backup /etc/hosts & /etc/hostname files
 cp /etc/hosts /root/hosts.old
-cp /etc/hosts /root/hostname.old
+cp /etc/hostname /root/hostname.old
 
 # Replace the string "metasploitable" with $newhost variable in the file /root/newhostfile.txt
+# This is used for testing purposes
+#sed -i "s/metasploitable/$newhost/g" /root/testme
 sed -i "s/metasploitable/$newhost/g" /etc/hosts
 
 # NOTE there is a Carriage Return Line Feed that is added in sed for some reason in this distro of Ubuntu 8.04...
 # It needs to be removed!
 # Remove carriage return line feed from /root/newhostsfile.txt and overwrite the /etc/hosts file
-tr -d '\r' < /etc/hosts > /etc/hosts
+tr -d '\r' < /etc/hosts > /root/hosts.new
+cat /root/hosts.new > /etc/hosts
 
 # Remove carriage return line feed from /root/newname.txt and overwrite the /etc/hostname file
-tr -d '\r' < /root/newname.txt > /etc/hostname
+echo $newhost > /root/notnewname.txt
+tr -d '\r' < /root/notnewname.txt > /root/hostname.new
+cat /root/hostname.new > /etc/hostname
+
+# Update hostname now
+updatehostname=$(cat /etc/hostname)
+hostname $updatehostname
+
+# Reset Monit ID, adding sleep random between 1 - 30 because we do not want to wait long
+randomnumber=$(echo $((1 + RANDOM % 30)))
+echo "Waiting $randomnumber Seconds to reset Monit ID"
+sleep $randomnumber && yes | monit -r
 
 # Delete this script after running once
-# rm -r /root/generate_random_hostname.sh
+rm -r /root/generate_random_hostname.sh
+
+# Reboot machine
+reboot
