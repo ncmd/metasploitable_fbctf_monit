c076c41283f30a67cfac47f5df0a6dd96b75a1bc
Charles M. Chong <cchong.vise@gmail.com>
2016-06-08 21:42:19 -0700

    Added backup feature

--- metasploitable/root/generate_random_hostname.sh
+++ metasploitable/root/generate_random_hostname.sh
@@ -14,13 +14,17 @@ sort -R /root/hostnamelist.txt | head -n 1 > /root/newname.txt
 # New hostname is declared by concatenating the contents of /root/newname.txt file
 newhost=$(cat /root/newname.txt)
 
+# Backup /etc/hosts & /etc/hostname files
+cp /etc/hosts /root/hosts.old
+cp /etc/hosts /root/hostname.old
+
 # Replace the string "metasploitable" with $newhost variable in the file /root/newhostfile.txt
-sed -i "s/metasploitable/$newhost/g" /root/newhostsfile.txt
+sed -i "s/metasploitable/$newhost/g" /etc/hosts
 
 # NOTE there is a Carriage Return Line Feed that is added in sed for some reason in this distro of Ubuntu 8.04...
 # It needs to be removed!
 # Remove carriage return line feed from /root/newhostsfile.txt and overwrite the /etc/hosts file
-tr -d '\r' < /root/newhostsfile.txt > /etc/hosts
+tr -d '\r' < /etc/hosts > /etc/hosts
 
 # Remove carriage return line feed from /root/newname.txt and overwrite the /etc/hostname file
 tr -d '\r' < /root/newname.txt > /etc/hostname
